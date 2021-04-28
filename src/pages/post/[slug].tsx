f7913f6c7d210c379229b3fc8c65c29b06819777
Bruno Gayet <brunogayet@gmail.com>
2021-04-28 15:03:48 -0400

    Semi-finished project

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,6 +1,13 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import Head from 'next/head';
+import { RichText } from 'prismic-dom';
+import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
+import Prismic from '@prismicio/client';
+import { formatDate } from '../../util/format';
 
 import { getPrismicClient } from '../../services/prismic';
+
+import Header from '../../components/Header';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
@@ -26,20 +33,118 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post({ post }: PostProps): JSX.Element {
+  // Counting words
+  const readingTime = post.data.content.reduce(
+    (acc, element) => {
+      if (element.heading) {
+        acc.totalWords += element.heading.split(/\s+/).length;
+      }
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+      if (element.body) {
+        acc.totalWords += RichText.asText(element.body).split(/\s+/).length;
+      }
 
-//   // TODO
-// };
+      return acc;
+    },
+    {
+      totalWords: 0,
+    }
+  );
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
+  const readingAvgPerMinute = 200;
 
-//   // TODO
-// };
+  return (
+    <>
+      <Head>
+        <title>{post.data.title} | Spacetraveling.</title>
+      </Head>
+
+      <Header />
+
+      <div
+        className={styles.banner}
+        style={{
+          background: `url(${post.data.banner.url}) center / cover no-repeat`,
+        }}
+      />
+
+      <main className={commonStyles.container}>
+        <article className={styles.postContainer}>
+          <h1>{post.data.title}</h1>
+
+          <div
+            className={`${commonStyles.informationsPost} ${styles.informationsPost}`}
+          >
+            <time>
+              <FiCalendar size={20} /> {post.first_publication_date}
+            </time>
+            <span>
+              <FiUser size={20} /> {post.data.author}
+            </span>
+            <span>
+              <FiClock size={20} />{' '}
+              {Math.ceil(readingTime.totalWords / readingAvgPerMinute)} min
+            </span>
+          </div>
+
+          <div className={styles.content}>
+            {post.data.content.map(element => (
+              <section key={element.heading} className={styles.elementContent}>
+                <h2>{element.heading}</h2>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: RichText.asHtml(element.body),
+                  }}
+                />
+              </section>
+            ))}
+          </div>
+        </article>
+      </main>
+    </>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient();
+  const posts = await prismic.query(
+    [Prismic.predicates.at('document.type', 'posts')],
+    {
+      fetch: ['post.uid'],
+      orderings: '[document.first_publication_date desc]',
+      pageSize: 2,
+    }
+  );
+
+  const paths = posts.results.map(post => ({
+    params: { slug: post.uid },
+  }));
+
+  return { paths, fallback: true };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params;
+
+  const prismic = getPrismicClient();
+  const response = await prismic.getByUID('posts', String(slug), {});
+
+  const post = {
+    first_publication_date: formatDate(response.first_publication_date),
+    data: {
+      title: response.data.title,
+      banner: {
+        url: response.data.banner.url,
+      },
+      author: response.data.author,
+      content: response.data.content,
+    },
+  };
+
+  return {
+    props: {
+      post,
+    },
+  };
+};
